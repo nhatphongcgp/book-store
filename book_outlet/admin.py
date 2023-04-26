7f0e5b55c7664352bc30f3ba535ec6dabfa70dff
Nguyen Phong <nguyenphong@Nguyens-MacBook-Pro.local>
2023-04-26 16:08:07 +0700

    Add relationship

--- book_outlet/admin.py
+++ book_outlet/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Book
+from .models import Book, Author, Address, Country
 
 
 class BookAdmin(admin.ModelAdmin):
@@ -12,3 +12,9 @@ class BookAdmin(admin.ModelAdmin):
 
 # Register your models here
 admin.site.register(Book, BookAdmin)
+
+admin.site.register(Author)
+
+admin.site.register(Address)
+
+admin.site.register(Country)
